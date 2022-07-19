eff4de8e182fcc1141ecdadf13318b10aa4aac4d
mlacerda97 <44003311+mlacerda97@users.noreply.github.com>
2022-07-19 17:22:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ function updateClock() {
     let minute = now.getMinutes();
     let second = now.getSeconds();
 
-    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}` // DIGITAL
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}` 
 
-    let sDeg = ((360 / 60) * second) - 90; // SEGUNDOS ANALOGICO
+    let sDeg = ((360 / 60) * second) - 90; 
     sElement.style.transform = `rotate(${sDeg}deg)`
 
-    let mDeg = ((360 / 60) * minute) - 90; // MINUTOS ANALOGICO
+    let mDeg = ((360 / 60) * minute) - 90; 
     mElement.style.transform = `rotate(${mDeg}deg)`
 
-    let hDeg = ((360/12) * hour) + ((30/60) * minute) - 90; /*Entre uma hora e outra temos um ângulo de 30 graus, o ponteiro percorrerá esse ângulo dentro dos 60 mins, logo a cada minuto o ponteiro percorrerá 0,5 grau. O minuto vai somente até 59 quando vai entrar no '60' ele vai pra próxima hora sendo o minuto 00, sendo assim não da nenhum bug.*/
+    let hDeg = ((360/12) * hour) + ((30/60) * minute) - 90; 
     hElement.style.transform = `rotate(${hDeg}deg)`
 }
 
@@ -29,5 +29,5 @@ function fixZero(time) {
     }
 }
 
-setInterval(updateClock, 1000); //1 segundo atualizar
-updateClock(); //Não ter delay
+setInterval(updateClock, 1000); 
+updateClock(); 
